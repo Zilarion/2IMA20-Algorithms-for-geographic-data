9c2a49fe82a8935047a502d73d6156acd9a295f8
Ruud Andriessen <ruud.andriessen@gmail.com>
2017-03-22 15:50:46 +0100

    Canvasified

--- visualize/qtree/index.js
+++ visualize/qtree/index.js
@@ -8,7 +8,7 @@ window.initMap = function() {
         var google = window.google;
         function SVGOverlay (map) {
             this.map = map;
-            this.svg = null;
+            this.canvas = null;
             this.quadtree = null;
 
             this.onPan = this.onPan.bind(this);
@@ -19,40 +19,43 @@ window.initMap = function() {
         SVGOverlay.prototype = new google.maps.OverlayView();
 
         SVGOverlay.prototype.onAdd = function () {
-            this.svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-            this.svg.style.position = 'absolute';
-            this.svg.style.top = 0;
-            this.svg.style.left = 0;
-            this.svg.style.width = '100%';
-            this.svg.style.height = '100%';
-            this.svg.style.pointerEvents = 'none';
+            let base = d3.select("#vis");
+            this.canvas = base.append("canvas")
+                .attr("width", "1920")
+                .attr("height", "1080")
+                .style("position", "absolute")
+                .style("top", 0)
+                .style("left", 0)
+                .style("width", "100%")
+                .style("height", "100%")
+                .style("pointer-events", "none");
 
             const bounds = this.map.getBounds();
-
             const proj = this.getProjection();
             this.quadtree = data;
 
-            generate_quadtree_map(this.svg, this.quadtree, proj);
+            paintCanvas(this.canvas, this.quadtree, proj, bounds);
 
             this.onPan();
-            document.body.appendChild(this.svg);
             this.map.addListener('center_changed', this.onPan);
         };
 
         SVGOverlay.prototype.onPan = function () {
-            let proj = this.getProjection();
-            redraw_qt(this.svg, this.quadtree, proj);
+            const proj = this.getProjection();
+            const bounds = this.map.getBounds();
+            paintCanvas(this.canvas, this.quadtree, proj, bounds);
         };
 
         SVGOverlay.prototype.onRemove = function () {
-            this.map.removeListener('center_changed', this.onPan);
-            this.svg.parentNode.removeChild(this.svg);
-            this.svg = null;
+            this.canvas.removeListener('center_changed', this.onPan);
+            this.canvas.parentNode.removeChild(this.canvas);
+            this.canvas = null;
         };
 
         SVGOverlay.prototype.draw = function () {
-            let proj = this.getProjection();
-            generate_quadtree_map(this.svg, this.quadtree, proj);
+            const proj = this.getProjection();
+            const bounds = this.map.getBounds();
+            paintCanvas(this.canvas, this.quadtree, proj, bounds);
         };
 
         // Create the Google Map…
@@ -66,64 +69,42 @@ window.initMap = function() {
     });
 };
 
+function paintCanvas(canvas, data, proj, bounds) {
+    // Resize canvas to screen size
+    canvas
+        .attr("width", canvas.node().offsetWidth)
+        .attr("height", canvas.node().offsetHeight);
+
+    // get the canvas drawing context and the width and height
+    const context = canvas.node().getContext('2d');
+    let cWidth = canvas.node().width;
+    let cHeight = canvas.node().height;
+
+    // clear the canvas from previous drawing
+    context.clearRect(0, 0, cWidth, cHeight);
+
+    // Draw all rects
+    data.forEach((d) => {
+        // if (bounds.contains(transformXY(proj, d.x1, d.y1)) ||
+        //     bounds.contains(transformXY(proj, d.x2, d.y1)) ||
+        //     bounds.contains(transformXY(proj, d.x1, d.y2)) ||
+        //     bounds.contains(transformXY(proj, d.x2, d.y2))) {
+        //     return;
+        // }
+        if (d.depth < 8) { return ;}
+        const p1 = transformXY(proj, d.x1, d.y1);
+        const p2 = transformXY(proj, d.x2, d.y2);
+
+        // console.log(p1.x, p2.y, (p2.x - p1.x), (p1.y-p2.y));
+        context.beginPath();
+        context.rect(p1.x, p2.y, p2.x - p1.x, p1.y - p2.y);
+        context.fillStyle = "rgba(255, 0, 0, 0.1)";
+        context.fill();
+        context.closePath();
+    });
+}
+
+
 function transformXY(proj, x, y) {
     return proj.fromLatLngToContainerPixel(new google.maps.LatLng(x, y));
-}
-
-function generate_quadtree_map(svg, quadtree_nodes, proj) {
-    d3.select(svg)
-        .selectAll("*").remove();
-
-    let node = d3.select(svg)
-        .selectAll(".node")
-        .data(quadtree_nodes);
-
-    let col = d3.scale.linear()
-        .domain([0, 40])
-        .range(['green', 'red']);
-
-    node.enter().append("rect")
-    //
-        .attr("x", function (d) {
-            return transformXY(proj, d.x1, d.y1).x;
-        })
-        .attr("y", function (d) {
-            return transformXY(proj, d.x2, d.y2).y;
-        })
-        .attr("width", function (d) {
-            let p1 = transformXY(proj, d.x1, d.y1);
-            let p2 = transformXY(proj, d.x2, d.y2);
-
-            return p2.x - p1.x;
-        })
-        .attr("height", function (d) {
-            let p1 = transformXY(proj, d.x1, d.y1);
-            let p2 = transformXY(proj, d.x2, d.y2);
-            return p1.y - p2.y;
-        })
-        .style("position", "absolute")
-        .attr('fill-opacity', function (d) {
-            return (d.depth / 50);
-        })
-        .style("fill", (d) => col(d.depth))
-        .attr("class", "node");
-    node.exit().remove();
-}
-
-function redraw_qt(svg, quadtree_nodes, proj) {
-    let node = d3.select(svg)
-        .selectAll(".node")
-        .data(quadtree_nodes)
-        .attr("x", function (d) {
-            return transformXY(proj, d.x1, d.y1).x;
-        })
-        .attr("y", function (d) {
-            return transformXY(proj, d.x2, d.y2).y;
-        });
-
-    // d3.select(svg)
-    //     .selectAll(".point")
-    //     .data(points)
-    //     .attr("cx", (d) => transformXY(proj, d.x, d.y).x)
-    //     .attr("cy", (d) => transformXY(proj, d.x, d.y).y)
 }
