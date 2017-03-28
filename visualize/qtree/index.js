1fcff215cb5fdf33c3785d00bcfd5f96746c84e8
Ruud Andriessen <ruud.andriessen@gmail.com>
2017-03-28 17:19:01 +0200

    Pretty

--- visualize/qtree/index.js
+++ visualize/qtree/index.js
@@ -76,7 +76,87 @@ window.initMap = function() {
         const map = new google.maps.Map(d3.select("#map").node(), {
             zoom: 11,
             center: new google.maps.LatLng(40.7, -73.975),
-            mapTypeId: google.maps.MapTypeId.TERRAIN
+            mapTypeId: google.maps.MapTypeId.TERRAIN,
+            styles: [
+                {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+                {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+                {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+                {
+                    featureType: 'administrative.locality',
+                    elementType: 'labels.text.fill',
+                    stylers: [{color: '#d59563'}]
+                },
+                {
+                    featureType: 'poi',
+                    elementType: 'labels.text.fill',
+                    stylers: [{color: '#d59563'}]
+                },
+                {
+                    featureType: 'poi.park',
+                    elementType: 'geometry',
+                    stylers: [{color: '#263c3f'}]
+                },
+                {
+                    featureType: 'poi.park',
+                    elementType: 'labels.text.fill',
+                    stylers: [{color: '#6b9a76'}]
+                },
+                {
+                    featureType: 'road',
+                    elementType: 'geometry',
+                    stylers: [{color: '#38414e'}]
+                },
+                {
+                    featureType: 'road',
+                    elementType: 'geometry.stroke',
+                    stylers: [{color: '#212a37'}]
+                },
+                {
+                    featureType: 'road',
+                    elementType: 'labels.text.fill',
+                    stylers: [{color: '#9ca5b3'}]
+                },
+                {
+                    featureType: 'road.highway',
+                    elementType: 'geometry',
+                    stylers: [{color: '#746855'}]
+                },
+                {
+                    featureType: 'road.highway',
+                    elementType: 'geometry.stroke',
+                    stylers: [{color: '#1f2835'}]
+                },
+                {
+                    featureType: 'road.highway',
+                    elementType: 'labels.text.fill',
+                    stylers: [{color: '#f3d19c'}]
+                },
+                {
+                    featureType: 'transit',
+                    elementType: 'geometry',
+                    stylers: [{color: '#2f3948'}]
+                },
+                {
+                    featureType: 'transit.station',
+                    elementType: 'labels.text.fill',
+                    stylers: [{color: '#d59563'}]
+                },
+                {
+                    featureType: 'water',
+                    elementType: 'geometry',
+                    stylers: [{color: '#17263c'}]
+                },
+                {
+                    featureType: 'water',
+                    elementType: 'labels.text.fill',
+                    stylers: [{color: '#515c6d'}]
+                },
+                {
+                    featureType: 'water',
+                    elementType: 'labels.text.stroke',
+                    stylers: [{color: '#17263c'}]
+                }
+            ]
         });
 
         let overlay = new SVGOverlay(map);
@@ -118,7 +198,7 @@ function paintCanvas(canvas, data, proj, bounds, zoom) {
         if (data.hasOwnProperty(level)) {
             if (level < zoom) {
                 const quads = data[level];
-                context.fillStyle = "rgba(0, 0, 0, " + (0.2 * level / data.maxLevel) + ")";
+                context.fillStyle = "rgba(183,28,28, " + (0.4 * (level / data.maxLevel) * (level / data.maxLevel)) + ")";
                 context.beginPath();
                 quads.forEach((d) => {
                     if (bounds.contains(new google.maps.LatLng(d.x1, d.y1)) ||
